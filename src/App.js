cff380dd46132245ef0482781459afb04b1411ce
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2023-01-10 11:45:50 -0800

    day two

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@
 import React from 'react';
 import Header from './Header';
 import Main from './Main'
+import Footer from './Footer'
 
 
 
-
-// 2nd Class component
+// 2nd Class component - Sub class that will extend the parent class from the React Library
 class App extends React.Component{
   render(){
     return (
       <>
         <Header />
         <Main />
+        <Footer />
       </>
     )
   }
